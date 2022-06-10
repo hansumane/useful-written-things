97fdedf7e4d6fbf034afdc4e69a0a419c31a7df5
Creasure <yurxo@bk.ru>
2022-06-10 22:12:49 +0300

    updated sqrt

--- src/sqrt/sqtest.cpp
+++ src/sqrt/sqtest.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
 #include <iomanip>
+#include <limits>
+#include <cmath>
 
 
 double sq(double arg) {
-
-    try {
-        if (!(arg > 0)) {
-            throw arg;
-        }
+    if (arg < 0) {
+        return std::numeric_limits<double>::quiet_NaN();
     }
-    catch (double& e) {
-        std::cout << "Argument must be > 0, returning 0" << std::endl;
+    else if (arg == 0.0) {
         return 0.0;
     }
-
-    double n = arg;
-    double x = n;
-    for (unsigned i = 0; i < 8; ++i)
-        x = (x + n/x) / 2;
-    return x;
-
+    else {
+        double n = arg;
+        double x = n;
+        for (unsigned i = 0; i < 8; ++i)
+            x = (x + n/x) / 2;
+        return x;
+    }
 }
 
 
 int main() {
-    double number;
+    double number, result, real;
     std::cout << "Input number: ";
     std::cin >> number; std::cin.get();
-    std::cout << std::fixed << std::setprecision(10) << sq(number) << std::endl;
+    result = sq(number);
+    real = sqrt(number);
+    std::cout << std::fixed << std::setprecision(20)
+            << "my function:    " << result << std::endl;
+    std::cout << std::fixed << std::setprecision(20)
+            << "sqrt function:  " << real << std::endl;
     return 0;
 }

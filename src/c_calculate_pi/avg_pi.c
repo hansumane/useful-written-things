358e91369bdd351fe915424ce917061203e68acc
Creasure <yurxo@bk.ru>
2022-06-13 18:04:31 +0300

    made higher precision in pi calculation

--- src/c_calculate_pi/avg_pi.c
+++ src/c_calculate_pi/avg_pi.c
@@ -5,8 +5,8 @@
  * in the [0, 1] range, which is actually pi / 4  | the proof is trivial :p
  * So as precision gets bigger, and step gets smaller,
  * average of sqrt(1 - x^2) gets closer to pi / 4.
- * This program uses 2^32 + 1 steps from 0 to 1,
- * and calculates 9 significant digits of pi within 12 seconds.
+ * This program uses 2^34 + 1 steps from 0 to 1,
+ * and calculates 10 significant digits of pi within 1 minute.
  * However, this is a very unefficient and slow method
  * to calculate pi, I designed it when I had no idea what
  * Taylor expansion is at all, nevertheless, it works, so... */
@@ -15,7 +15,7 @@ int main(void) {
 
     const double one = 1.0l, four = 4.0l;
     const double start = 0.0l, end = 1.0l;
-    const long long unsigned precision = pow(2llu, 32llu);
+    const long long unsigned precision = pow(2llu, 34llu);
     const long long unsigned amount = precision + 1llu;
     const double step = (end - start) / precision;
     double pi = 0.0l;
@@ -24,9 +24,9 @@ int main(void) {
         pi += four * sqrt(one - x*x);
 
     pi /= amount;
-    printf("pi = %.10lf\n"
-           "              ^\n"
-           "  9 significant digits are calculated correctly\n", pi);
+    printf("pi = %.11lf\n"
+           "               ^\n"
+           "  10 significant digits are calculated correctly\n", pi);
 
     return 0;
 
--- src/c_calculate_pi/taylor_pi.c
+++ src/c_calculate_pi/taylor_pi.c
@@ -3,13 +3,13 @@
 /* This program calculates pi by expanding atan(1) to a Taylor series,
  * which is 1 - 1/3 + 1/5 - 1/7 + ... or sum((-1)^n/(2*n + 1), 0, N).
  * So as N gets bigger, this sum gets closer to pi / 4.
- * This program uses 2^32 as N and calculates 9 significant digits of pi
- * within approximately 10 seconds total */
+ * This program uses 2^34 as N and calculates 10 significant digits of pi
+ * within approximately 40 seconds total */
 
 int main (void) {
 
     const double four = 4.0l; /* multiplying every series member */
-    const unsigned precision = 32u; /* means 2 ^ 32 operations */
+    const unsigned precision = 34u; /* means 2 ^ 34 operations */
     double pi = 0.0l;
     double sign = 1.0l;
     double denominator = 1.0l;
@@ -21,9 +21,9 @@ int main (void) {
         denominator += 2.0l;
     }
 
-    printf("pi = %.10lf\n"
-           "              ^\n"
-           "  9 significant digits are calculated correctly\n", pi);
+    printf("pi = %.11lf\n"
+           "               ^\n"
+           "  10 significant digits are calculated correctly\n", pi);
 
     return 0;
 

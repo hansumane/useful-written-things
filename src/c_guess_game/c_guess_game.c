2321b7d751b3efd37a0a02beae2afcb61280d0f9
Creasure <yurxo@bk.ru>
2022-07-10 17:58:31 +0300

    added out-* to gitignore and changed rng to /dev/random in c_guess_game

--- src/c_guess_game/c_guess_game.c
+++ src/c_guess_game/c_guess_game.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void) {
+int true_random(int min, int max)
+{
+	unsigned temp;
+	FILE *dev_random = fopen("/dev/random", "rb");
+	fread(&temp, sizeof(temp), 1, dev_random);
+	fclose(dev_random);
+	int result = temp % (max - min + 1) + min;
+	return result;
+}
 
-    srand(time(0));
+int main(void)
+{
+	int guess, value = true_random(1, 100);
 
-    unsigned guess, value = rand() % 100 + 1;
+	do {
+		printf("Guess the value between 1 and 100: ");
+		scanf("%d", &guess); getchar();
+		if (guess > value)
+			printf("Your guess is bigger >!\n");
+		else if (guess < value)
+			printf("Your guess is smaller <!\n");
+		else
+			printf("Congratulations! You have guessed %u!\n", value);
+	} while (guess != value);
 
-    do {
-        printf("Guess the value between 1 and 100: ");
-        scanf("%u", &guess); getchar();
-        if (guess > value)
-            printf("Your guess is bigger >!\n");
-        else if (guess < value)
-            printf("Your guess is smaller <!\n");
-        else
-            printf("Congratulations! You have guessed %u!\n", value);
-    } while (guess != value);
-
-    return 0;
-
+	return 0;
 }
